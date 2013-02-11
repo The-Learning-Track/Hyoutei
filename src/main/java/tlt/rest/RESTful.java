e38d38299887a99655d611a7c6414725b6727d8d
Katsu K <kkawakam@bu.edu>
2013-02-11 00:16:04 -0500

    Renamed File

---
+++ src/main/java/tlt/rest/RESTful.java
@@ -0,0 +1,32 @@
+package tlt.rest;
+ 
+import java.rmi.RemoteException;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+import tlt.queryapp.QueryApp;
+ 
+@Path("/hello")
+public class RESTful {
+ 
+	@GET
+	@Path("/{param}")
+	public Response getMsg(@PathParam("param") String msg) {
+		/* Missing Inputs, going to replace this with SpringSource Application Context */
+		QueryApp query = new QueryApp();
+		String output = null;
+		try{
+			output = query.sendCourseQuery();
+		}
+		catch(RemoteException e){
+			
+		}
+ 
+		return Response.status(200).entity(output).build();
+ 
+	}
+ 
+}
