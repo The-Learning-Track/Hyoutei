8e73a49fd27caca7272646ffd256e2d272d722b8
Katsu K <kkawa1570@gmail.com>
2013-04-04 19:51:16 -0400

    Added ScheduledExecutorService to run periodic login and logout method

--- src/main/java/tlt/startup/StartupServletContext.java
+++ src/main/java/tlt/startup/StartupServletContext.java
@@ -1,5 +1,9 @@
 package tlt.startup;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
@@ -7,6 +11,7 @@ import tlt.queryapp.QueryApp;
 
 public class StartupServletContext implements ServletContextListener {
 	private QueryApp application;
+	private ScheduledExecutorService scheduler;
 
 	public QueryApp getApplication() {
 		return application;
@@ -14,18 +19,21 @@ public class StartupServletContext implements ServletContextListener {
 
 	@Override
 	public void contextDestroyed(ServletContextEvent arg0) {
+		scheduler.shutdownNow();
 		if(application.logOut() == true)
 			System.out.println("Logout Sucessful");
 		else
 			System.out.println("Logout Unsuccessful");
 
 	}
-
 	@Override
+
 	public void contextInitialized(ServletContextEvent event) {
 		application = new QueryApp();
 		
 		event.getServletContext().setAttribute("Queryapp", application);
+	    scheduler = Executors.newSingleThreadScheduledExecutor();
+	    scheduler.scheduleAtFixedRate(new ExtendSessionTime(application), 60, 300, TimeUnit.SECONDS);
 		if(application.initializeSOAPhandle() == true){
 			System.out.println("SOAP Handler Initialization Successfull");
 			if(application.login()== true)
